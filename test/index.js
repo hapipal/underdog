728638b3ace7a267630f81afd2aa218c4b9203d1
Devin Ivy <devin@bigroomstudios.com>
2016-09-03 14:22:00 -0400

    Scaffold for publish/tests

---
+++ test/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('lab');
+const Code = require('code');
+const Hapi = require('hapi');
+const Http2 = require('http2');
+const Underdog = require('..');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const before = lab.before;
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+const internals = {};
+
+describe('Underdog', () => {
+
+    it('', (done) => {
+
+        done();
+    });
+});
