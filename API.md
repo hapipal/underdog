6f3d6fbaba58b08e32cf94c82c8c1e636ccc254b
Devin Ivy <devin@bigroomstudios.com>
2018-01-08 14:49:26 -0500

    Incorporate pushAllowed, tidy docs, etc.

---
+++ API.md
@@ -0,0 +1,20 @@
+# API
+## `Underdog`
+### The hapi plugin
+You should register Underdog in any plugin that would like to take advantage of its features; it does not take any options.  Underdog specifies the `once` [plugin attribute](https://github.com/hapijs/hapi/blob/master/API.md#plugins), which means hapi will ensure it is not registered multiple times.
+
+Note, Underdog utilizes an `onPreResponse` extension to finalize server-push, so if you modify the response in an `onPreResponse` extension and would like that response to push additional resources, you must specify `{ before: 'underdog' }` as an option when calling [`server.ext()`](https://github.com/hapijs/hapi/blob/master/API.md#server.ext()).
+
+#### `h.push([response], path, [headers])`
+
+ - `response` - the hapi [response](https://github.com/hapijs/hapi/blob/master/API.md#response-object) for which you'd like to push additional resources.  Should not be an error response.  When this argument is omitted Underdog will attempt to use the request's currently set response, which will fail with an error if there is no such response.
+ - `path` - the path to another resource living on the same connection as the current request that you'd like to server-push.  If the path is relative (does not begin with `/`) then it is adjusted based upon the relevant [realm's](https://github.com/hapijs/hapi/blob/master/API.md#server.realm) route prefix.
+ - `headers` - any headers that you would like to add to the push-request for this resource.  By default a user-agent header is automatically added to match the user-agent of the originating request.
+
+ Returns an object `{ response, allowed }` where `response` is the response for which resources are intended to be pushed, and `allowed` indicates whether the server-push was allowed by the client and protocol (when `false`, no resources were pushed).
+
+ Note that when push requests are resolved credentials from the originating request will be used, and that routes marked as `isInternal` are accessible.
+
+#### `h.pushAllowed()`
+
+Returns `true` or `false`, indicating whether server-push is allowed by the client and protocol.  Sometimes resources cannot be pushed, for example when the server handles an http/1.1 request or when an http/2 client disables pushes.  See [ALPN Negotiation](https://nodejs.org/api/http2.html#http2_alpn_negotiation) and [HTTP/2 Session Settings](https://nodejs.org/api/http2.html#http2_settings_object) in the nodejs docs for more info.
--- test/index.js
+++ test/index.js
@@ -7,6 +7,8 @@ const Code = require('code');
 const Hapi = require('hapi');
 const Boom = require('boom');
 const Toys = require('toys');
+const Http = require('http');
+const Https = require('https');
 const Http2 = require('http2');
 const Underdog = require('..');
 
@@ -21,17 +23,12 @@ const internals = {};
 
 describe('Underdog', () => {
 
-    const makeServer = async (noStart, routes) => {
+    const creds = {
+        key: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA0UqyXDCqWDKpoNQQK/fdr0OkG4gW6DUafxdufH9GmkX/zoKz\ng/SFLrPipzSGINKWtyMvo7mPjXqqVgE10LDI3VFV8IR6fnART+AF8CW5HMBPGt/s\nfQW4W4puvBHkBxWSW1EvbecgNEIS9hTGvHXkFzm4xJ2e9DHp2xoVAjREC73B7JbF\nhc5ZGGchKw+CFmAiNysU0DmBgQcac0eg2pWoT+YGmTeQj6sRXO67n2xy/hA1DuN6\nA4WBK3wM3O4BnTG0dNbWUEbe7yAbV5gEyq57GhJIeYxRvveVDaX90LoAqM4cUH06\n6rciON0UbDHV2LP/JaH5jzBjUyCnKLLo5snlbwIDAQABAoIBAQDJm7YC3pJJUcxb\nc8x8PlHbUkJUjxzZ5MW4Zb71yLkfRYzsxrTcyQA+g+QzA4KtPY8XrZpnkgm51M8e\n+B16AcIMiBxMC6HgCF503i16LyyJiKrrDYfGy2rTK6AOJQHO3TXWJ3eT3BAGpxuS\n12K2Cq6EvQLCy79iJm7Ks+5G6EggMZPfCVdEhffRm2Epl4T7LpIAqWiUDcDfS05n\nNNfAGxxvALPn+D+kzcSF6hpmCVrFVTf9ouhvnr+0DpIIVPwSK/REAF3Ux5SQvFuL\njPmh3bGwfRtcC5d21QNrHdoBVSN2UBLmbHUpBUcOBI8FyivAWJhRfKnhTvXMFG8L\nwaXB51IZAoGBAP/E3uz6zCyN7l2j09wmbyNOi1AKvr1WSmuBJveITouwblnRSdvc\nsYm4YYE0Vb94AG4n7JIfZLKtTN0xvnCo8tYjrdwMJyGfEfMGCQQ9MpOBXAkVVZvP\ne2k4zHNNsfvSc38UNSt7K0HkVuH5BkRBQeskcsyMeu0qK4wQwdtiCoBDAoGBANF7\nFMppYxSW4ir7Jvkh0P8bP/Z7AtaSmkX7iMmUYT+gMFB5EKqFTQjNQgSJxS/uHVDE\nSC5co8WGHnRk7YH2Pp+Ty1fHfXNWyoOOzNEWvg6CFeMHW2o+/qZd4Z5Fep6qCLaa\nFvzWWC2S5YslEaaP8DQ74aAX4o+/TECrxi0z2lllAoGAdRB6qCSyRsI/k4Rkd6Lv\nw00z3lLMsoRIU6QtXaZ5rN335Awyrfr5F3vYxPZbOOOH7uM/GDJeOJmxUJxv+cia\nPQDflpPJZU4VPRJKFjKcb38JzO6C3Gm+po5kpXGuQQA19LgfDeO2DNaiHZOJFrx3\nm1R3Zr/1k491lwokcHETNVkCgYBPLjrZl6Q/8BhlLrG4kbOx+dbfj/euq5NsyHsX\n1uI7bo1Una5TBjfsD8nYdUr3pwWltcui2pl83Ak+7bdo3G8nWnIOJ/WfVzsNJzj7\n/6CvUzR6sBk5u739nJbfgFutBZBtlSkDQPHrqA7j3Ysibl3ZIJlULjMRKrnj6Ans\npCDwkQKBgQCM7gu3p7veYwCZaxqDMz5/GGFUB1My7sK0hcT7/oH61yw3O8pOekee\nuctI1R3NOudn1cs5TAy/aypgLDYTUGQTiBRILeMiZnOrvQQB9cEf7TFgDoRNCcDs\nV/ZWiegVB/WY7H0BkCekuq5bHwjgtJTpvHGqQ9YD7RhE8RSYOhdQ/Q==\n-----END RSA PRIVATE KEY-----\n',
+        cert: '-----BEGIN CERTIFICATE-----\nMIIDBjCCAe4CCQDvLNml6smHlTANBgkqhkiG9w0BAQUFADBFMQswCQYDVQQGEwJV\nUzETMBEGA1UECAwKU29tZS1TdGF0ZTEhMB8GA1UECgwYSW50ZXJuZXQgV2lkZ2l0\ncyBQdHkgTHRkMB4XDTE0MDEyNTIxMjIxOFoXDTE1MDEyNTIxMjIxOFowRTELMAkG\nA1UEBhMCVVMxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoMGEludGVybmV0\nIFdpZGdpdHMgUHR5IEx0ZDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\nANFKslwwqlgyqaDUECv33a9DpBuIFug1Gn8Xbnx/RppF/86Cs4P0hS6z4qc0hiDS\nlrcjL6O5j416qlYBNdCwyN1RVfCEen5wEU/gBfAluRzATxrf7H0FuFuKbrwR5AcV\nkltRL23nIDRCEvYUxrx15Bc5uMSdnvQx6dsaFQI0RAu9weyWxYXOWRhnISsPghZg\nIjcrFNA5gYEHGnNHoNqVqE/mBpk3kI+rEVzuu59scv4QNQ7jegOFgSt8DNzuAZ0x\ntHTW1lBG3u8gG1eYBMquexoSSHmMUb73lQ2l/dC6AKjOHFB9Ouq3IjjdFGwx1diz\n/yWh+Y8wY1Mgpyiy6ObJ5W8CAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAoSc6Skb4\ng1e0ZqPKXBV2qbx7hlqIyYpubCl1rDiEdVzqYYZEwmst36fJRRrVaFuAM/1DYAmT\nWMhU+yTfA+vCS4tql9b9zUhPw/IDHpBDWyR01spoZFBF/hE1MGNpCSXXsAbmCiVf\naxrIgR2DNketbDxkQx671KwF1+1JOMo9ffXp+OhuRo5NaGIxhTsZ+f/MA4y084Aj\nDI39av50sTRTWWShlN+J7PtdQVA5SZD97oYbeUeL7gI18kAJww9eUdmT0nEjcwKs\nxsQT1fyKbo7AlZBY4KSlUMuGnn0VnAsB9b+LxtXlDfnjyM8bVQx1uAfRo0DO8p/5\n3J5DTjAU55deBQ==\n-----END CERTIFICATE-----\n'
+    };
 
-        if (typeof routes === 'undefined') {
-            routes = noStart;
-            noStart = false;
-        }
-
-        const creds = {
-            key: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA0UqyXDCqWDKpoNQQK/fdr0OkG4gW6DUafxdufH9GmkX/zoKz\ng/SFLrPipzSGINKWtyMvo7mPjXqqVgE10LDI3VFV8IR6fnART+AF8CW5HMBPGt/s\nfQW4W4puvBHkBxWSW1EvbecgNEIS9hTGvHXkFzm4xJ2e9DHp2xoVAjREC73B7JbF\nhc5ZGGchKw+CFmAiNysU0DmBgQcac0eg2pWoT+YGmTeQj6sRXO67n2xy/hA1DuN6\nA4WBK3wM3O4BnTG0dNbWUEbe7yAbV5gEyq57GhJIeYxRvveVDaX90LoAqM4cUH06\n6rciON0UbDHV2LP/JaH5jzBjUyCnKLLo5snlbwIDAQABAoIBAQDJm7YC3pJJUcxb\nc8x8PlHbUkJUjxzZ5MW4Zb71yLkfRYzsxrTcyQA+g+QzA4KtPY8XrZpnkgm51M8e\n+B16AcIMiBxMC6HgCF503i16LyyJiKrrDYfGy2rTK6AOJQHO3TXWJ3eT3BAGpxuS\n12K2Cq6EvQLCy79iJm7Ks+5G6EggMZPfCVdEhffRm2Epl4T7LpIAqWiUDcDfS05n\nNNfAGxxvALPn+D+kzcSF6hpmCVrFVTf9ouhvnr+0DpIIVPwSK/REAF3Ux5SQvFuL\njPmh3bGwfRtcC5d21QNrHdoBVSN2UBLmbHUpBUcOBI8FyivAWJhRfKnhTvXMFG8L\nwaXB51IZAoGBAP/E3uz6zCyN7l2j09wmbyNOi1AKvr1WSmuBJveITouwblnRSdvc\nsYm4YYE0Vb94AG4n7JIfZLKtTN0xvnCo8tYjrdwMJyGfEfMGCQQ9MpOBXAkVVZvP\ne2k4zHNNsfvSc38UNSt7K0HkVuH5BkRBQeskcsyMeu0qK4wQwdtiCoBDAoGBANF7\nFMppYxSW4ir7Jvkh0P8bP/Z7AtaSmkX7iMmUYT+gMFB5EKqFTQjNQgSJxS/uHVDE\nSC5co8WGHnRk7YH2Pp+Ty1fHfXNWyoOOzNEWvg6CFeMHW2o+/qZd4Z5Fep6qCLaa\nFvzWWC2S5YslEaaP8DQ74aAX4o+/TECrxi0z2lllAoGAdRB6qCSyRsI/k4Rkd6Lv\nw00z3lLMsoRIU6QtXaZ5rN335Awyrfr5F3vYxPZbOOOH7uM/GDJeOJmxUJxv+cia\nPQDflpPJZU4VPRJKFjKcb38JzO6C3Gm+po5kpXGuQQA19LgfDeO2DNaiHZOJFrx3\nm1R3Zr/1k491lwokcHETNVkCgYBPLjrZl6Q/8BhlLrG4kbOx+dbfj/euq5NsyHsX\n1uI7bo1Una5TBjfsD8nYdUr3pwWltcui2pl83Ak+7bdo3G8nWnIOJ/WfVzsNJzj7\n/6CvUzR6sBk5u739nJbfgFutBZBtlSkDQPHrqA7j3Ysibl3ZIJlULjMRKrnj6Ans\npCDwkQKBgQCM7gu3p7veYwCZaxqDMz5/GGFUB1My7sK0hcT7/oH61yw3O8pOekee\nuctI1R3NOudn1cs5TAy/aypgLDYTUGQTiBRILeMiZnOrvQQB9cEf7TFgDoRNCcDs\nV/ZWiegVB/WY7H0BkCekuq5bHwjgtJTpvHGqQ9YD7RhE8RSYOhdQ/Q==\n-----END RSA PRIVATE KEY-----\n',
-            cert: '-----BEGIN CERTIFICATE-----\nMIIDBjCCAe4CCQDvLNml6smHlTANBgkqhkiG9w0BAQUFADBFMQswCQYDVQQGEwJV\nUzETMBEGA1UECAwKU29tZS1TdGF0ZTEhMB8GA1UECgwYSW50ZXJuZXQgV2lkZ2l0\ncyBQdHkgTHRkMB4XDTE0MDEyNTIxMjIxOFoXDTE1MDEyNTIxMjIxOFowRTELMAkG\nA1UEBhMCVVMxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoMGEludGVybmV0\nIFdpZGdpdHMgUHR5IEx0ZDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\nANFKslwwqlgyqaDUECv33a9DpBuIFug1Gn8Xbnx/RppF/86Cs4P0hS6z4qc0hiDS\nlrcjL6O5j416qlYBNdCwyN1RVfCEen5wEU/gBfAluRzATxrf7H0FuFuKbrwR5AcV\nkltRL23nIDRCEvYUxrx15Bc5uMSdnvQx6dsaFQI0RAu9weyWxYXOWRhnISsPghZg\nIjcrFNA5gYEHGnNHoNqVqE/mBpk3kI+rEVzuu59scv4QNQ7jegOFgSt8DNzuAZ0x\ntHTW1lBG3u8gG1eYBMquexoSSHmMUb73lQ2l/dC6AKjOHFB9Ouq3IjjdFGwx1diz\n/yWh+Y8wY1Mgpyiy6ObJ5W8CAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAoSc6Skb4\ng1e0ZqPKXBV2qbx7hlqIyYpubCl1rDiEdVzqYYZEwmst36fJRRrVaFuAM/1DYAmT\nWMhU+yTfA+vCS4tql9b9zUhPw/IDHpBDWyR01spoZFBF/hE1MGNpCSXXsAbmCiVf\naxrIgR2DNketbDxkQx671KwF1+1JOMo9ffXp+OhuRo5NaGIxhTsZ+f/MA4y084Aj\nDI39av50sTRTWWShlN+J7PtdQVA5SZD97oYbeUeL7gI18kAJww9eUdmT0nEjcwKs\nxsQT1fyKbo7AlZBY4KSlUMuGnn0VnAsB9b+LxtXlDfnjyM8bVQx1uAfRo0DO8p/5\n3J5DTjAU55deBQ==\n-----END CERTIFICATE-----\n'
-        };
+    const makeServer = async (routes) => {
 
         const server = Hapi.server({
             debug: false,
@@ -41,12 +38,8 @@ describe('Underdog', () => {
 
         await server.register(Underdog);
 
-        if (routes && routes.length) {
+        if (routes) {
             server.route(routes);
-        }
-
-        if (noStart) {
-            return { server };
         }
 
         await server.start();
@@ -84,7 +77,7 @@ describe('Underdog', () => {
         );
     };
 
-    it('pushes complete resource for an explicitly specified response.', { plan: 7 }, async (flags) => {
+    it('pushes complete resource for an explicitly specified response.', { plan: 9 }, async (flags) => {
 
         const { server, client } = await makeServer([
             {
@@ -92,9 +85,12 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
                     const result = h.push(response, '/push-me');
-                    expect(result).to.shallow.equal(response);
+
+                    expect(result).to.only.contain(['allowed', 'response']);
+                    expect(result.allowed).to.equal(true);
+                    expect(result.response).to.shallow.equal(response);
 
                     return response;
                 }
@@ -141,7 +137,7 @@ describe('Underdog', () => {
         await expectToStream(pushed, 'pushed');
     });
 
-    it('pushes resource for the request\'s current response.', { plan: 6 }, async (flags) => {
+    it('pushes resource for the request\'s current response.', { plan: 7 }, async (flags) => {
 
         const { server, client } = await makeServer([
             {
@@ -153,8 +149,9 @@ describe('Underdog', () => {
                         onPostHandler: {
                             method: (request, h) => {
 
-                                const response = h.push('/push-me');
-                                expect(response).to.shallow.equal(request.response);
+                                const result = h.push('/push-me');
+                                expect(result.allowed).to.equal(true);
+                                expect(result.response).to.shallow.equal(request.response);
 
                                 return h.continue;
                             }
@@ -370,7 +367,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = h.push(Boom.notFound(), '/push-me');
+                    const { response } = h.push(Boom.notFound(), '/push-me');
 
                     return response;
                 }
@@ -407,7 +404,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
 
                     h.push(response, '/push-me', {
                         'x-custom': 'pooh',
@@ -464,7 +461,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
                     h.push(response, '/push-me');
 
                     return response;
@@ -475,7 +472,7 @@ describe('Underdog', () => {
                 path: '/push-me',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('pushed');
+                    const response = h.response('pushed');
                     h.push(response, '/push-me-chain');
 
                     return response;
@@ -541,7 +538,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
                     h.push(response, '/push-me', { 'x-custom': 'pooh' });
 
                     return response;
@@ -609,7 +606,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
                     h.push(response, '/push-me');
 
                     expect(request.auth.isAuthenticated).to.equal(true);
@@ -674,7 +671,7 @@ describe('Underdog', () => {
                         path: '/',
                         handler: (request, h) => {
 
-                            const response = request.generateResponse('body');
+                            const response = h.response('body');
                             h.push(response, 'push-me');
 
                             return response;
@@ -732,7 +729,7 @@ describe('Underdog', () => {
                 path: '/',
                 handler: (request, h) => {
 
-                    const response = request.generateResponse('body');
+                    const response = h.response('body');
                     h.push(response, 'push-me');
 
                     return response;
@@ -774,5 +771,218 @@ describe('Underdog', () => {
 
         await expectToStream(request, 'body');
         await expectToStream(pushed, 'pushed');
+    });
+
+    it('h.push() indicates if push was not allowed.', { plan: 4 }, async (flags) => {
+
+        const { server, client: ignore } = await makeServer([
+            {
+                method: 'get',
+                path: '/',
+                handler: (request, h) => {
+
+                    const response = h.response('body');
+                    const result = h.push(response, '/push-me');
+
+                    expect(result).to.only.contain(['allowed', 'response']);
+                    expect(result.allowed).to.equal(false);
+                    expect(result.response).to.shallow.equal(response);
+
+                    return response;
+                }
+            },
+            {
+                method: 'get',
+                path: '/push-me',
+                handler: (request) => 'pushed'
+            }
+        ]);
+
+        ignore.destroy();
+
+        const client = Http2.connect(server.info.uri, {
+            rejectUnauthorized: false,
+            settings: { enablePush: false }
+        });
+
+        flags.onCleanup = async () => {
+
+            client.destroy();
+            await server.stop();
+        };
+
+        const request = client.request({ ':path': '/' });
+
+        await expectToStream(request, 'body');
+    });
+
+    describe('h.pushAllowed()', () => {
+
+        it('returns false when not using an http2 server.', async (flags) => {
+
+            const server = Hapi.server();
+
+            flags.onCleanup = async () => await server.stop();
+
+            await server.register(Underdog);
+
+            server.route({
+                method: 'get',
+                path: '/',
+                handler: (request, h) => h.pushAllowed()
+            });
+
+            await server.start();
+
+            const req = Http.get(`${server.info.uri}`);
+            const res = await Toys.event(req, 'response');
+
+            await expectToStream(res, 'false');
+        });
+
+        it('returns false when falling back to http1.', async (flags) => {
+
+            const server = Hapi.server({
+                tls: true,
+                listener: Http2.createSecureServer({
+                    ...creds,
+                    allowHTTP1: true
+                })
+            });
+
+            flags.onCleanup = async () => await server.stop();
+
+            await server.register(Underdog);
+
+            server.route({
+                method: 'get',
+                path: '/',
+                handler: (request, h) => h.pushAllowed()
+            });
+
+            await server.start();
+
+            const req = Https.get({
+                href: server.info.uri,
+                port: server.info.port,
+                agent: new Https.Agent({ rejectUnauthorized: false })
+            });
+
+            const res = await Toys.event(req, 'response');
+
+            await expectToStream(res, 'false');
+        });
+
+        it('returns false when using http2 but the client doesn\'t support push.', async (flags) => {
+
+            const server = Hapi.server({
+                tls: true,
+                listener: Http2.createSecureServer(creds)
+            });
+
+            await server.register(Underdog);
+
+            server.route({
+                method: 'get',
+                path: '/',
+                handler: (request, h) => h.pushAllowed()
+            });
+
+            await server.start();
+
+            const client = Http2.connect(server.info.uri, {
+                rejectUnauthorized: false,
+                settings: { enablePush: false }
+            });
+
+            flags.onCleanup = async () => {
+
+                client.destroy();
+                await server.stop();
+            };
+
+            const res = client.request({ ':path': '/' });
+
+            await expectToStream(res, 'false');
+        });
+
+        it('returns true when using http2 and the client supports push.', async (flags) => {
+
+            const server = Hapi.server({
+                tls: true,
+                listener: Http2.createSecureServer(creds)
+            });
+
+            await server.register(Underdog);
+
+            server.route({
+                method: 'get',
+                path: '/',
+                handler: (request, h) => h.pushAllowed()
+            });
+
+            await server.start();
+
+            const client = Http2.connect(server.info.uri, {
+                rejectUnauthorized: false
+            });
+
+            flags.onCleanup = async () => {
+
+                client.destroy();
+                await server.stop();
+            };
+
+            const res = client.request({ ':path': '/' });
+
+            await expectToStream(res, 'true');
+        });
+
+        it('works when called in a pushed request.', async (flags) => {
+
+            const server = Hapi.server({
+                tls: true,
+                listener: Http2.createSecureServer(creds)
+            });
+
+            await server.register(Underdog);
+
+            server.route([
+                {
+                    method: 'get',
+                    path: '/',
+                    handler: (request, h) => {
+
+                        const response = h.response('body');
+                        h.push(response, '/push-me');
+
+                        return response;
+                    }
+                },
+                {
+                    method: 'get',
+                    path: '/push-me',
+                    handler: (request, h) => h.pushAllowed()
+                }
+            ]);
+
+            await server.start();
+
+            const client = Http2.connect(server.info.uri, {
+                rejectUnauthorized: false
+            });
+
+            flags.onCleanup = async () => {
+
+                client.destroy();
+                await server.stop();
+            };
+
+            client.request({ ':path': '/' });
+
+            const [[pushed]] = await getPushes(client);
+
+            await expectToStream(pushed, 'true');
+        });
     });
 });
